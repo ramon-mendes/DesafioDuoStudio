68a97788c37105f0411c31ed5a2d470ab9ea412c
Ramon Mendes <midiway.sc@gmail.com>
2021-01-06 18:43:20 -0300

    first commit

---
+++ DesafioDuoStudio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesafioDuoStudio.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
